1f3e1a31c1c8fb3872617cdbc03ba83d43b4db4c
minshim <87307266+minshim@users.noreply.github.com>
2024-08-08 14:47:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// ----------------------------------------------------------------logo rotation
+
+function swingOnScroll() {
+  const scrollTop = window.scrollY;
+  const swingElementA = document.querySelector('.logo_A');
+  const swingElementB = document.querySelector('.logo_B');
+  const swingElementC = document.querySelector('.logo_C');
+  const maxRotationA = 60; 
+  const maxRotationB = 270; 
+  const maxRotationC = 460; 
+  const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+  let rotationA = (scrollTop / scrollHeight) * maxRotationA; 
+  let rotationB = (scrollTop / scrollHeight) * maxRotationB; 
+  let rotationC = (scrollTop / scrollHeight) * maxRotationC; 
+  
+  rotationA *= 0.5; 
+  rotationB *= 0.9;
+  rotationC *= 0.8; 
+  
+  swingElementA.style.transform = `rotate(${rotationA}deg)`;
+  swingElementB.style.transform = `rotate(${rotationB}deg)`;
+  swingElementC.style.transform = `rotate(${rotationC}deg)`;
+  }
+  
+  function hideLogoOnClick(logoElement) {
+  logoElement.style.transition = 'opacity 0.1s'; 
+  logoElement.style.opacity = '0'; 
+  setTimeout(function() {
+  logoElement.style.display = 'none'; 
+  }, 1000); 
+  }
+  
+  document.querySelector('.logo_A').addEventListener('click', function() {
+  hideLogoOnClick(this);
+  });
+  
+  document.querySelector('.logo_B').addEventListener('click', function() {
+  hideLogoOnClick(this);
+  });
+  
+  document.querySelector('.logo_C').addEventListener('click', function() {
+  hideLogoOnClick(this);
+  });
+  
+  window.addEventListener('scroll', swingOnScroll);
+  
+  
